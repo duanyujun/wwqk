bebd6ddf46b26aa84a7dcff6194649dade3d91da
duanyujun <duanyujun@gmail.com>
2016-11-16 18:04:34 +0800

    commit

---
+++ src/com/wwqk/model/ShooterAssistsSource.java
@@ -0,0 +1,13 @@
+package com.wwqk.model;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class ShooterAssistsSource extends Model<ShooterAssistsSource> {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static final ShooterAssistsSource dao = new ShooterAssistsSource();
+	
+}
