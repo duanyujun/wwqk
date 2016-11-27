7e7db515e8883b2b6c1773a5a99581ecb9c8ffc4
JAMES <JAMES@JAMES-PC>
2016-11-27 11:20:38 +0800

    commit

--- src/com/wwqk/constants/CommonConstants.java
+++ src/com/wwqk/constants/CommonConstants.java
@@ -7,4 +7,9 @@ public class CommonConstants {
 	
 	public static final int DEFAULT_RANK_SIZE = 50;
 	
+	/** 小图片需替换 大小1675 */
+	public static final int SMALL_IMG_LENGTH = 2000;
+	
+	/** 大图片需替换 大小8624 */
+	public static final int BIG_IMG_LENGTH = 10000;
 }
--- src/com/wwqk/utils/ImageUtils.java
+++ src/com/wwqk/utils/ImageUtils.java
@@ -1,10 +1,15 @@
 package com.wwqk.utils;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Date;
+
+import javax.imageio.ImageIO;
 
 import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
@@ -136,6 +141,35 @@ public class ImageUtils {
 		}
 		fileName = fileName.replaceAll("\\\\", "/");
 		return fileName;
+	}
+	
+	/** 
+	 * 改变图片的大小到宽为size，然后高随着宽等比例变化 
+	 * @param is 上传的图片的输入流 
+	 * @param os 改变了图片的大小后，把图片的流输出到目标OutputStream 
+	 * @param size 新图片的宽 
+	 * @param format 新图片的格式 
+	 * @throws IOException 
+	 */  
+	public static void resizeImage(InputStream is, OutputStream os, int size, String format){  
+	    BufferedImage prevImage;
+		try {
+			prevImage = ImageIO.read(is);
+			double width = prevImage.getWidth();  
+		    double height = prevImage.getHeight();  
+		    double percent = size/width;  
+		    int newWidth = (int)(width * percent);  
+		    int newHeight = (int)(height * percent);  
+		    BufferedImage image = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_BGR);  
+		    Graphics graphics = image.createGraphics();  
+		    graphics.drawImage(prevImage, 0, 0, newWidth, newHeight, null);  
+		    ImageIO.write(image, format, os);  
+		    os.flush();  
+		    is.close();  
+		    os.close(); 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}  
 	}
 	
 	public static void main(String[] args) {
