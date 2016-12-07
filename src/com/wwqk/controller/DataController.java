721e8d64c7e13d754d35201e7af6b67eadc3b40b
duanyujun <duanyujun@gmail.com>
2016-12-07 18:00:01 +0800

    commit

--- src/com/wwqk/controller/DataController.java
+++ src/com/wwqk/controller/DataController.java
@@ -22,7 +22,7 @@ public class DataController extends Controller {
 		List<LeaguePosition> positionList = LeaguePosition.dao.find("select * from league_position where league_id = ? ORDER BY rank ASC ", leagueId);
 		setAttr("positionList", positionList);
 		
-		List<LeagueShooter> shooterList = LeagueShooter.dao.find("select * from league_shooter where league_id = ? ORDER BY rank ASC ", leagueId);
+		List<LeagueShooter> shooterList = LeagueShooter.dao.find("select * from league_shooter where league_id = ? ORDER BY goal_count desc, penalty_count asc ", leagueId);
 		setAttr("shooterList", shooterList);
 		
 		List<LeagueAssists> assistsList = LeagueAssists.dao.find("select * from league_assists where league_id = ? ORDER BY rank ASC ", leagueId);
