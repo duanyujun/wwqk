a55f007de4ae3d90cea5874425eb5e828c4757c3
duanyujun <duanyujun@gmail.com>
2017-10-02 23:04:52 +0800

    commit

---
+++ src/com/wwqk/constants/AHAmountEnum.java
@@ -0,0 +1,66 @@
+package com.wwqk.constants;
+
+import com.wwqk.utils.IEnum;
+
+public enum AHAmountEnum implements IEnum{
+	PS("1", "0"), 
+	PSBQ("2", "-0.25"),
+	BQ("3", "-0.5"),
+	BQYQ("4", "-0.75"),
+	YQ("5", "-1"),
+	YQQB("6", "-1.25"), 
+	QB("7", "-1.5"),
+	QBLQ("8", "-1.75"),
+	LQ("9", "-2"),
+	LQQB("10", "-2.25"),
+	LQB("11", "-2.5"),
+	LQSQ("12", "-2.75"),
+	SQ("13", "-3"), 
+	
+	MPSBQ("102", "0.25"),
+	MBQ("103", "0.5"),
+	MBQYQ("104", "0.75"),
+	MYQ("105", "1"),
+	MYQQB("106", "1.25"), 
+	MQB("107", "1.5"),
+	MQBLQ("108", "1.75"),
+	MLQ("109", "2"),
+	MLQQB("110", "2.25"),
+	MLQB("111", "2.5"),
+	MLQSQ("112", "2.75"),
+	MSQ("113", "3"),
+	
+	OTHERS("1000","其他");
+	
+	/** 键 */
+	private String key;
+
+	/** 值 */
+	private String value;
+	
+	private AHAmountEnum(String key, String value) {
+		this.key = key;
+		this.value = value;	
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	@Override
+	public String getName() {
+		return this.name();
+	}
+}
