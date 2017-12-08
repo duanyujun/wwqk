71be8e34558131ae01e134d100789c26a87c17dd
duanyujun <duanyujun@gmail.com>
2017-12-08 14:48:39 +0800

    commit

---
+++ src/com/wwqk/job/VideosJob.java
@@ -0,0 +1,23 @@
+package com.wwqk.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import com.wwqk.utils.VideosZuqiulaUtils;
+
+public class VideosJob implements Job {
+	
+	@Override
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		VideosZuqiulaUtils.collect(false);
+		VideosZuqiulaUtils.updateDesc();
+	}
+	
+}
+
+
+
+
+
+
